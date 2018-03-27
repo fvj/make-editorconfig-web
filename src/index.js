9cba2e79a745623cb0fad40f36fb6b7597fe7350
Julius von Froreich <fvj@users.noreply.github.com>
2018-03-27 18:31:18 +0200

    use custom css
    make the top-margin 30px

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { render } from 'react-dom'
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
+import './css/custom.css'
 import App from './app'
 
 render(<App />, document.getElementById('app'))
